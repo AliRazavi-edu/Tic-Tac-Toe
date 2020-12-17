13a3c6f1e63ac3d21db48a0bca5c7d112d5ddbcb
alirazavi <seyed.ali.razavi.ebrahimi@gmail.com>
2020-12-17 04:08:43 +0330

    Tic Tac Toe

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+          {/*
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -16,7 +17,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
